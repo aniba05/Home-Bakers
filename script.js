3bbaef55a8fcc282e1cb3d7f36b850665b0b360c
Baskaran Ramasamy <anithaanitha2639@gmail.com>
2024-07-23 20:11:01 +0530

    commit for home bakers

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var slides = document.querySelectorAll('.carousel-slide');
+    var currentSlide = 0;
+    var slideInterval = setInterval(nextSlide, 5000); // Change slide every 5 seconds
+  
+    function nextSlide() {
+      slides[currentSlide].classList.remove('active');
+      currentSlide = (currentSlide + 1) % slides.length;
+      slides[currentSlide].classList.add('active');
+    }
+  });
